5560529fe9627736840c3e37ddad39b4a3b707de
DOLBIMLED <50545147+Userteck@users.noreply.github.com>
2023-02-03 12:37:23 +0200

    commit
    update 03.02.23

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+</head>
 <?php
-echo 'parbaude';
+    echo 'parbaude';
 ?>
+  <body>
+    
+  </body>
+</html>
+
